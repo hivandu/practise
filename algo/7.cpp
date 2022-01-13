d87776caa2bd4f7917cac0c803617a93ac3583c9
Hivan Du <doo@hivan.me>
2022-01-13 15:16:12 +0800

    update

--- algo/7.cpp
+++ algo/7.cpp
@@ -87,3 +87,109 @@ int find(char* a, int n, char key) {
         return i;
     }
 }
+
+// 反转链表
+public ListNode reverseList(ListNode head) {
+    ListNode prev = null;
+    ListNode curr = head;
+    while(curr != null) {
+        ListNode nextTemp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = nextTemp;
+    }
+    return prev;
+}
+
+// 迭代反转链表
+link * iteration_reverse(link* head) {
+    if(head == NULL || head -> next == NULL) {
+        return head;
+    }
+    else{
+        link * beg = NULL;
+        link * mid = head;
+        link * end = head -> next;
+
+        // 一直遍历
+        while(1) {
+            // 修改mid所指结点的指向
+            mid->next = beg;
+            // 此时判断end是否为NULL, 如果成立则退出循环
+            if (end == NULL) {
+                break;
+            }
+            // 整体向后移动3个指针
+            beg = mid;
+            mid = end;
+            end = end->next;
+        }
+        // 最后修改head头指针的指向
+        head = mid;
+        return head;
+    }
+}
+
+// 递归反转链表
+link* recursive_reverse(link* head) {
+    // 递归的出口
+    if (head == NULL || head->next == NULL) // 空链或只有一个结点, 直接返回头指针
+    {
+        return head;
+    }
+    else
+    {
+        // 一直递归, 找到链表中最后一个结点
+        link *new_head = recursive_reverse(head->next);
+
+        //当逐层退出时，new_head 的指向都不变，一直指向原链表中最后一个节点；
+        //递归每退出一层，函数中 head 指针的指向都会发生改变，都指向上一个节点。
+        //每退出一层，都需要改变 head->next 节点指针域的指向，同时令 head 所指节点的指针域为 NULL。
+        head->next->next = head;
+        head->next = NULL;
+        //每一层递归结束，都要将新的头指针返回给上一层。由此，即可保证整个递归过程中，能够一直找得到新链表的表头。
+        return new_head;
+    }
+}
+
+// 头插法反转链表
+
+link * head_reverse(link * head) {
+    link * new_head = NULL;
+    link * temp = NULL;
+
+    if (head == NULL || head->next == NULL) {
+        return head;
+    }
+    while(head != NULL){
+        temp = head;
+        //  将temp从head中摘除
+        head = head->next;
+
+        // 将temp插入到new_head的头部
+        temp->next = new_head;
+        new_head = temp;
+    }
+    return new_head;
+}
+
+// 就地逆置法反转链表
+link * local_reverse(link * head) {
+    link * beg = NULL;
+    link * end = NULL;
+    if (head == NULL || head->next == NULL) {
+        return head;
+    }
+    beg = head;
+    end = head -> next;
+    while (end != NULL) {
+        // 将end从链表中摘除
+        beg->next = end->next;
+        // 将end移动至链表头
+        end->next = head;
+        head = end;
+        // 调整end的指向, 另其指向beg后的一个结点, 为反转下一个结点做准备
+        end = beg->next;
+    }
+    return head;
+}
