9cb6eb1b14cd285ae09d4d478c850b1114836f21
Hivan Du <doo@hivan.me>
2022-01-13 15:15:45 +0800

    update 1.test.c

---
+++ C++/1.test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+
+// 请使用printf函数, 求解一个数字n的十进制表示的数字位数
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", printf("%d", n));
+    return 0;
+}
---
+++ C++/hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello World\n");
+    printf("There are %u apples\n", 3);
+    printf("%s says it is %i o'clock\n", "Ben", 3);
+    printf("%5d\n", 123);
+    printf("%-5d\n", 123);
+    printf("%12f\n", 123.45);
+    return 0;
+}
+
+
+return printf("123");
