ee48779ec788f20cd0f9a853a6efd4cf02813607
Hivan Du <doo@hivan.me>
2022-02-14 14:13:14 +0800

    update

--- C++/hello.cpp
+++ C++/hello.cpp
@@ -1,3 +1,10 @@
+/*
+ * @Author: Hivan Du
+ * @mail: doo@hivan.me
+ * @LastEditors: Hivan Du
+ * @LastEditTime: 2022-02-14 14:13:05
+ */
+// 随便更新点啥!
 #include <iostream>
 int main() {
     std::cout << "Hello world\n";
